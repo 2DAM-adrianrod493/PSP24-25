89eebc20a54f27423e632e1fa9cf619a000fe7c2
2DAM-adrianrod493 <arodrod0708@g.educaand.es>
2024-09-20 17:53:02 +0200

    1

--- psp-t1-01/src/Main.java
+++ psp-t1-01/src/Main.java
@@ -1,12 +1,10 @@
-import org.w3c.dom.ls.LSOutput;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Otra línea más");
+
     }
-
-    System.out.println("otra línea más");
-
 
 }
